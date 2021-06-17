da44344dc7e87c34555b9958b484ffa36129e2c2
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-06-17 13:00:47 -0300

    Criado testes com fakeAsync para esperar determinado tempo ou controlar o tempo de execução

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
 import { PhotoFrameModule } from './photo-frame.module';
 
@@ -18,4 +23,29 @@ describe(PhotoFrameComponent.name, () => {
   it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name} Should trigger (@Output liked) onde when called multiple times within debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => {
+      times++;
+    });
+    component.like();
+    component.like();
+    tick(500);
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name} Should trigger (@Output liked) two times when called outside debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => {
+      times++;
+    });
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
+    expect(times).toBe(2);
+  }));
 });
