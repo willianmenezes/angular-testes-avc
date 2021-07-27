2bb221666755ff6a86efd16090f51cb55df99179
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-07-27 14:18:22 -0300

    Simulando o clique com o botão enter

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -54,4 +54,20 @@ describe(LikeWidgetComponent.name, () => {
 
     likeWidgetContainerEl.click();
   });
+
+  it('(D) Should display number of likes when ENTER key is pressed', (done) => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      component.likes++;
+      fixture.detectChanges();
+      const counterEl: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+      expect(counterEl.textContent.trim()).toBe('1');
+      done();
+    });
+
+    const likeWidgetContainerEl: HTMLElement = fixture.nativeElement.querySelector('.like-widget-container');
+
+    const event = new KeyboardEvent('keyup', { key: 'Enter' });
+    likeWidgetContainerEl.dispatchEvent(event);
+  });
 });
